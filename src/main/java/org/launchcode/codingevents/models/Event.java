93fe19dbc91c28ac30c0c20497af8f000834645a
Sally Steuterman <sally.steuterman@gmail.com>
2023-07-28 12:26:12 -0500

    Updating controller and model to handle errors

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -14,13 +14,14 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
-    @NotBlank
+    @NotBlank(message = "Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
 
     @Size(max = 500, message = "Description too long!")
     private String description;
 
+    @NotBlank(message = "Email is required")
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
@@ -32,6 +33,7 @@ public class Event {
         nextId++;
     }
 
+    public Event() {}
     public String getName() {
         return name;
     }
