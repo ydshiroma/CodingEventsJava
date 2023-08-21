bb651239bb86fdcc9ae9391345116caca366ac26
John Woolbright <john@pop-os.localdomain>
2023-08-21 11:31:53 -0500

    persistent-model branch complete
    updated gradle version and spring version for compatibility

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,8 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -9,10 +12,12 @@ import java.util.Objects;
 /**
  * Created by Chris Bay
  */
+@Entity
 public class Event {
 
+    @Id
+    @GeneratedValue
     private int id;
-    private static int nextId = 1;
 
     @NotBlank(message = "Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
@@ -28,21 +33,17 @@ public class Event {
     private EventType type;
 
     public Event(String name, String description, String contactEmail, EventType type) {
-        this();
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
         this.type = type;
     }
 
-    public Event() {
-        this.id = nextId;
-        nextId++;
-    }
+    public Event() {}
+
     public String getName() {
         return name;
     }
-
 
     public void setName(String name) {
         this.name = name;
@@ -64,9 +65,13 @@ public class Event {
         this.contactEmail = contactEmail;
     }
 
-    public EventType getType() { return type; }
+    public EventType getType() {
+        return type;
+    }
 
-    public void setType(EventType type) { this.type = type; }
+    public void setType(EventType type) {
+        this.type = type;
+    }
 
     public int getId() {
         return id;
